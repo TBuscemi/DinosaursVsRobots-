452e7a6e37dec9797485807fe153a2c22cec9027
TBuscemi <thomasbuscemi124@gmail.com>
2021-07-14 18:03:22 -0400

    I started to add name,hp and attack to my first dinosaur to test it

--- herd.py
+++ herd.py
@@ -1,7 +1,13 @@
+from dinosaur import Dinosaur
+
+
 class Herd:
 
 
-    def __init__(self, dino_names):
-        dino_names = dino_names
+    def __init__(self, dino_profile):
+        dino_profile = dino_profile
+
+
+        dino_profile =[Dinosaur("REX" ,100 ,25)]
 
         
