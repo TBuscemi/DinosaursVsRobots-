059173775424000f3ecd8a8316d163611b792f05
TBuscemi <thomasbuscemi124@gmail.com>
2021-07-16 09:08:39 -0400

    just changed the attack names for the dino side

--- herd.py
+++ herd.py
@@ -5,8 +5,8 @@ class Herd:
 
 
     def __init__(self):
-        self.dino_profile = [Dinosaur("Tyrannosaurus rex AKA The king" ,100 , Weapon("claws",25)),
-                            Dinosaur("Velociraptors AKA The Pack" ,100 ,Weapon("cLAWS",25)),
-                            Dinosaur("Mosasaurus AKA The Big Chomp" ,100 ,Weapon("claws",25))]
+        self.dino_profile = [Dinosaur("Tyrannosaurus rex AKA The king" ,100 , Weapon("Tear!",25)),
+                            Dinosaur("Velociraptors AKA The Pack" ,100 ,Weapon("CLAWS",25)),
+                            Dinosaur("Mosasaurus AKA The Big Chomp" ,100 ,Weapon("CHOMP",25))]
 
         
